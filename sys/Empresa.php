9b77d2defa718d0c49d50a67415b765c79e5e129
Igor Raphael <igorraphaeel@live.com>
2019-07-01 17:51:41 -0300

    Mostrar dados da empresa para o usuario e se nao ouver dados, deixar o formulario ativado para poder preencher e enviar ao db.

--- sys/Empresa.php
+++ sys/Empresa.php
@@ -1,11 +1,42 @@
 <?php
 
-
-class EMpresa extends IncubaMain 
-{
-    public function teste(){
-        return "aaaa";
-    }
+class Empresa extends IncubaMain{
     
-   
+    /**
+    * Construtor para instanciar a classe banco de dados
+    */
+    public function __construct(){
+        $db = new Database();
+        $this->conn = $db->_connect();
+    }
+
+    /**
+    * Função para retornar os dados da empresa caso exista no db.
+    * @return $company = dados da empresa OU false.
+    */
+    public function getCompanyData(){
+        $sql = "SELECT * FROM empresa";
+        $res = $this->conn->query($sql);
+        if ($res->num_rows > 0) {
+            $row = $res->fetch_assoc();
+            $company = array(
+                'cnpj' => $row["cnpj_emp"],
+                'razao_social' => $row["razao_social"],
+                'inscricao_estadual' => $row["inscricao_estadual"],
+                'endereco' => $row["endereco"],
+                'bairro' => $row["bairro"],
+                'municipio' => $row["municipio"],
+                'cep' => $row["cep"],
+                'uf' => $row["uf"],
+                'nome_responsavel' => $row["nome_responsavel"],
+                'email_responsavel' => $row["email_responsavel"],
+                'cargo_responsavel' => $row["cargo_responsavel"],
+                'tel_responsavel' => $row["tel_responsavel"],
+            );
+            return $company; 
+        }else{
+            return false;
+        }
+
+    }
 }
