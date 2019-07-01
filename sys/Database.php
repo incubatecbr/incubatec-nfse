18b3dffb8b8f2dc9c100a14f4ba6f7fbf8503864
Igor Raphael <igorraphaeel@live.com>
2019-07-01 17:50:42 -0300

    Add classe database

--- sys/Database.php
+++ sys/Database.php
@@ -1,8 +1,31 @@
 <?php
 
-class Database{
-    
-    public function aaaaa(){
+class Database
+{
 
+    private $server = 'localhost';
+    private $user = 'root';
+    private $password = '';
+    private $db = 'incubatec_nf';
+
+    /**
+     * Função para connectar ao bd
+     * @return void
+     */
+    public function _connect(){
+        $conn = new mysqli($this->server, $this->user, $this->password, $this->db);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        return $conn;
     }
+
+    /**
+     * Função para fechar a conexao com o db
+     * @param $conn variavel de connection
+     */
+    public function _close($conn){
+        $conn->close();
+    }
+
 }
