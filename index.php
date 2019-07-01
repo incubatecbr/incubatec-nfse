ab687df4150931fb7d3e9dc2e72c4ea4ce2c1182
Igor Raphael <igorraphaeel@live.com>
2019-07-01 10:09:35 -0300

    Adicionado autoloader.php responsavel pelo carregamento automatico das calsses e index.php que verifica se existe post e chama a função responsavel

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+    $APP_PATH['root'] = $_SERVER['DOCUMENT_ROOT'] . '/incubatecnf/';
+    $APP_PATH['sys'] = $APP_PATH['root'] . 'sys/';
+    $APP_PATH['view'] = $APP_PATH['root'] . 'views/';
+
+    require_once('autoloader.php');
+    
+    if(isset($_POST['action']) && !empty($_POST['action'])  ){
+
+        $ctrl_name = ucfirst($_POST['mod']);
+        if(class_exists($ctrl_name)){
+            $_controller = new $ctrl_name();
+            $_response = $_controller->init();
+
+            if (isset($_response)) {
+                header('Cache-Control: no-store, no-cache, must-revalidate');
+                header('Expires: Wed, 05 Jun 1985 05:00:00 GMT');
+                header('Content-type: application/json; charset=utf-8');
+                echo json_encode($_response);
+                exit;
+            }
+        }
+    }else{
+        header('Content-type: text/html; charset=utf-8');
+        require_once('base.html');
+        exit;
+    }
+
+
+    
