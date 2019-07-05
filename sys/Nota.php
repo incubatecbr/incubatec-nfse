ab0a51f8fa09bf75bc1d1469751c3463876bf312
Igor Raphael <igorraphaeel@live.com>
2019-07-05 17:38:28 -0300

    Adicionado tela para nota fiscal

---
+++ sys/Nota.php
@@ -0,0 +1,16 @@
+<?php
+
+class Nota extends IncubaMain{
+    
+    /**
+     * Construtor para instanciar a classe banco de dados
+     */
+    public function __construct(){
+        $db = new Database();
+        $this->conn = $db->_connect();
+    }
+
+ 
+}
+
+
