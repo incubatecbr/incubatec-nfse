b1b0cc4f2959e58f910296d9f2b011139108f4af
Igor Raphael <igorraphaeel@live.com>
2019-07-09 10:58:48 -0300

    ALterações no formulario de NF

--- database.sql
+++ database.sql
@@ -13,3 +13,37 @@ CREATE TABLE `incubatec_nf`.`empresa` (
   `tel_responsavel` VARCHAR(12) NOT NULL,
   PRIMARY KEY (`cnpj_emp`),
   UNIQUE INDEX `cnpj_UNIQUE` (`cnpj_emp` ASC));
+
+
+  CREATE TABLE `incubatec_nf`.`nota_fiscal` (
+  `id_nota` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `num_nota` VARCHAR(9) NOT NULL,
+  `cpf_cnpj_cliente` VARCHAR(14) NOT NULL,
+  `razao_social` VARCHAR(35) NOT NULL,
+  `inscricao_estadual` VARCHAR(14) NOT NULL,
+  `razao_social` VARCHAR(35) NOT NULL,
+  `cod_consumidor_cliente` VARCHAR(12) NOT NULL,
+  `data_emissao_nf` VARCHAR(8) NOT NULL,
+  `ano_mes_apuracao` VARCHAR(4) NOT NULL,
+  `modelo` VARCHAR(2) NOT NULL,
+  `serie` VARCHAR(3) GENERATED ALWAYS AS (001) VIRTUAL,
+  `cfop` VARCHAR(4) NOT NULL,
+  `situacao_doc` VARCHAR(1) NOT NULL,
+  `referencia_item_nota` VARCHAR(9) NULL,
+  `telefone_cliente` VARCHAR(12) NULL,
+  `ind_tipo_cpf_cnpj` VARCHAR(1) NULL,
+  `tipo_cliente` VARCHAR(2) NOT NULL,
+  `telefone_empresa` VARCHAR(12) NULL,
+  `cnpj_emitente` VARCHAR(14) NOT NULL,
+  `valor_total_nf` VARCHAR(12) NOT NULL,
+  PRIMARY KEY (`id_nota`),
+  UNIQUE INDEX `num_nota_UNIQUE` (`num_nota` ASC));
+
+  CREATE TABLE `incubatec_nf`.`item_nota` (
+  `id_item_nota` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_nota` INT NOT NULL,
+  `cod_item` VARCHAR(10) NOT NULL,
+  `descricao` VARCHAR(40) NOT NULL,
+  `undade` VARCHAR(6) NULL DEFAULT 'UN     ',
+  PRIMARY KEY (`id_item_nota`));
+
