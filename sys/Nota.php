1c6f30fbc533b71a9019c421ce734a1cfc08bc02
Igor Raphael <igorraphaeel@live.com>
2019-07-12 16:05:45 -0300

    Funcionando a inserção de notas e de itens da nota no banco de dados

--- sys/Nota.php
+++ sys/Nota.php
@@ -10,66 +10,59 @@ class Nota extends IncubaMain{
         $this->conn = $db->_connect();
     }
 
-    public function insertNewNF(){
-        $service = $_POST['data2'];
-        
-        $data = Util::formatArray($_POST['dataFor']);
-        if ( strlen($data['cpf_cnpj']) < 12 ) {
-            $ind_tipo_cpf_cnpj = 2;
-        }else{
-            $ind_tipo_cpf_cnpj = 1;
-        }
+    public function newNotaFiscal(){
+        $ret = '';
+        $services = Util::formatArrayServices($_POST['data2']);//Array contendo o(s) serviço(s).
+        $data = Util::formatArray($_POST['dataFor']);//Array contendo os dados do formulário.
+        $ind_tipo_cpf_cnpj = (strlen($data['cpf_cnpj']) < 12 ) ? 2 : 1;//  2 para CPF, 1 para CNPJ.
         $lastNum = $this->getMaxNumberNota();
         $numNota = (!isset($lastNum) OR ($lastNum == null) ) ? "000000001" : Util::nextNumberOfNota($lastNum);
-
         $data_e = $this->_dataEnterprise();//Dados empresa
         $data_c = $this->_dataCliente($data['id']);//Dados do cliente.
-   
-        $vlrTotalNF = Util::sumValuesItens($service);
+        $vlrTotalNF = Util::sumValuesItens($services);
         $cnpj_e = Util::replaceString($data_e["cnpj"]);
         $telC = Util::replaceString($data_c["telefone"]);
         $telE = Util::replaceString($data_e["tel_responsavel"]);
-
-        //$sql = "INSERT INTO nota_fiscal(num_nota, cpf_cnpj_cliente, razao_social, inscricao_estadual, cod_consumidor_cliente, data_emissao_nf, ano_mes_apuracao, modelo, cfop, situacao_doc, referencia_item_nota, telefone_cliente, ind_tipo_cpf_cnpj, tipo_cliente, telefone_empresa, cnpj_emitente, valor_total_nf) VALUES('{$numNota}','{$data_c["cpf_cnpj"]}', '{$data_c["razao_social"]}', '{$data_c["inscricao_estadual"]}', '{$data_c["id"]}', '{$data["dt_emissao"]}', '{$data["ano_mes_apu"]}', '{$data["modelo"]}', '{$data["cfop"]}', '{$data["situacao_doc"]}', 'ref', '{$telC}', $ind_tipo_cpf_cnpj, '{$data["tipo_cliente"]}', '{$telE}', '{$cnpj_e}','{$vlrTotalNF}')";
-        
-        $new = [];
-        for ($i=0; $i < count($service); $i++) { 
-            if (is_array($service[$i])): 
-                //$new["itens"][$i] = $service[$i];
-                for ($j=0; $j < 6; $j++) { 
-                    //$new["itens"][$i] = $service[$j];
-                    //var_dump( $service[$i][$j] ) ;
-                    $new["itens"][$i] = $service[$i];
-                }
-            endif;
-        }
-        var_dump($new);
-        //
-        
-        // foreach ($service as $s => $val) {
-        //     //$sql = "INSERT INTO item_nota(id_nota, cod_item, descricao, valor_item) VALUES(``,``,``,``,``)";
-
-        //     print_r("{$val} <br>");
-        // }
-
-        die();
-
-
+        //sql
+        $sql = "INSERT INTO nota_fiscal(num_nota, cpf_cnpj_cliente, razao_social, inscricao_estadual, cod_consumidor_cliente, data_emissao_nf, ano_mes_apuracao, modelo, cfop, situacao_doc, referencia_item_nota, telefone_cliente, ind_tipo_cpf_cnpj, tipo_cliente, telefone_empresa, cnpj_emitente, valor_total_nf) VALUES('{$numNota}','{$data_c["cpf_cnpj"]}', '{$data_c["razao_social"]}', '{$data_c["inscricao_estadual"]}', '{$data_c["id"]}', '{$data["dt_emissao"]}', '{$data["ano_mes_apu"]}', '{$data["modelo"]}', '{$data["cfop"]}', '{$data["situacao_doc"]}', 'ref', '{$telC}', $ind_tipo_cpf_cnpj, '{$data["tipo_cliente"]}', '{$telE}', '{$cnpj_e}','{$vlrTotalNF}')";
+     
         if($this->conn->query($sql) === TRUE) {
-            //return true; //INSERIR ITENS DA NOTA
+            //INSERIR ITENS DA NOTA
             $last_id = $this->conn->insert_id;
-            
-            //return $last_id;
-            
+            $c_services = $this->newItemService($services, $last_id);
+            $ret = "Nota fiscal finalizada contendo {$c_services} serviço";
         }else{
-            return "Error {$this->conn->error}";
+            $ret = "Error {$this->conn->error}";
         }
         $this->conn->close();//fecha conexao com db.
-        
-        //return $sql;
+        return $ret;
     }
 
-    
+    public function newItemService($array, $last_id){
+        $c = 0;
+        if(is_array($array[0])){
+            for ($i=0; $i < count($array); $i++) { 
+                $sql = "INSERT INTO item_nota(id_nota, cod_item, descricao, valor_item) VALUES($last_id,'{$array[$i][0]}','{$array[$i][1]}','{$array[$i][2]}')";
+                if($this->conn->query($sql) === TRUE) {
+                    $c++;
+                }else{
+                    return "Error {$this->conn->error}";
+                    die();
+                }
+            }
+        }else{
+            $sql = "INSERT INTO item_nota(id_nota, cod_item, descricao, valor_item) VALUES($last_id,'{$array[0]}','{$array[1]}','{$array[2]}')";
+            if($this->conn->query($sql) === TRUE) {
+                $c++;
+            }else{
+                return "Error {$this->conn->error}";
+                die();
+            }
+        }
+        $this->conn->close();//fecha conexao com db.
+        return $c;
+    }
+
     /**
      * Função para retornar o maior id de nota fiscal.
      * @return $max Retorna nulll ou numero.
@@ -78,6 +71,7 @@ class Nota extends IncubaMain{
         $sql = "SELECT MAX(id_nota) AS numMax from nota_fiscal";
         $exe = $this->conn->query($sql);
         $result = $exe->fetch_assoc();
+        $this->conn->close();//fecha conexao com db.
         $max = ( !isset($result) ) ? null : $result['numMax'];
         return $max;
     }
--- sys/Util.php
+++ sys/Util.php
@@ -3,21 +3,26 @@
 class Util
 {
 
+    /**
+     * Função checa se no indice existe um array de dados
+     * caso exista, passa array para função de soma.
+     * @param Array $service
+     * @return $val Somatoria dos valores do array ou retorna o unico valor do array formatado.
+     */
     public static function sumValuesItens($service){
-        $services = 0;
+        $c_services = 0;
         for ($i = 0; $i < count($service); $i++){
             if (is_array($service[$i])): //verifica se o indice contem um array.
-                $services++; //contagem de quantos itens.
+                $c_services++; //contagem de quantos itens.
             endif;
         }
-        if(isset($services) && $services != 0){
-            //return $services;
+        if(isset($c_services) && $c_services != 0){
             $val = Util::sumValues($service);
             return $val;
         }else{
             //return $service[2];
             return str_replace (',', '', $service[2]);
-            // return number_format($service[2], 2, '', '');
+            //return number_format($service[2], 2, '', '');
         }    
     }
     
@@ -31,16 +36,36 @@ class Util
         return sprintf("%'.09d", $num);
     }
 
+    /**
+     * Função responsavel por somar os valores contidos no indice [2]
+     * @param Array $array
+     * @return String Somatoria dos valores Ex: 5000 (50,00).
+     */
     public static function sumValues($array){
         $sum = 0;
         for ($i = 0; $i < count($array); $i++) {
             $sb = str_replace(',', '.', $array[$i][2]); //remove virgula e ponto para realizar a soma dos valores.
             $sum += $sb;
         }
-        return number_format($sum, 2, '', '');
+        //return number_format($sum, 2, '', '');
+        return $sum;
     }
 
 
+    /**
+     * Função para formatar o array contendo os itens de serviço.
+     * @param Array $array para formatar.
+     * @return Array $newArray formatado.
+     */
+    public static function formatArrayServices($array){
+        $newArray = array();
+        for ($i=0; $i < count($array); $i++) { 
+            for ($j=0; $j < 6; $j++) { 
+                $newArray[$i] = Util::replaceString($array[$i]);
+            }
+        }
+        return $newArray;
+    }
     /**
      * Função para reorganizar o array. 
      * $newArray = ["name"] => ["value"];
